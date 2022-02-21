01b1c32b4b0f8aa51646c7300d33bbad3bba1bc1
サトウ <68115677+arincomamire@users.noreply.github.com>
2022-02-21 17:11:07 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+with open('index.html', mode ='r') as f:
+    index = f.read()
+
+class HelloServerHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(index.encode('utf-8'))
+        return
+
+server = HTTPServer(('', 8000), HelloServerHandler)
+server.serve_forever()
